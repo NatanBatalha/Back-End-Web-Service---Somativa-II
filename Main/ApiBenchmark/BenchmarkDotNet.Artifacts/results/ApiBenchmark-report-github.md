35d400407e53e6941ade0716ba4f101cdb233dfb
Natan Battaglia de Araújo <65867003+NatanBatalha@users.noreply.github.com>
2024-11-11 22:38:04 -0300

    Part II
    ...

---
+++ Main/ApiBenchmark/BenchmarkDotNet.Artifacts/results/ApiBenchmark-report-github.md
@@ -0,0 +1,16 @@
+```
+
+BenchmarkDotNet v0.14.0, Windows 11 (10.0.22631.4391/23H2/2023Update/SunValley3)
+AMD Ryzen 5 5600H with Radeon Graphics, 1 CPU, 12 logical and 6 physical cores
+.NET SDK 8.0.303
+  [Host]     : .NET 8.0.7 (8.0.724.31311), X64 RyuJIT AVX2
+  DefaultJob : .NET 8.0.7 (8.0.724.31311), X64 RyuJIT AVX2
+
+
+```
+| Method     | Mean | Error |
+|----------- |-----:|------:|
+| GetUsersMe |   NA |    NA |
+
+Benchmarks with issues:
+  ApiBenchmark.GetUsersMe: DefaultJob
---
+++ Main/ApiBenchmark/Program.cs
@@ -0,0 +1,75 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+
+//executa teste carga ---->>> dotnet run -c Release
+public class ApiBenchmark
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    // Lista de usuários com e-mails e senhas (dados diretamente no código)
+    private static List<User> users = new List<User>
+    {
+        new User { Email = "admin@authserver.com", Password = "admin" },
+        new User { Email = "gustavo@gmail.com", Password = "password" }
+    };
+
+    [Benchmark]
+    public async Task GetUsersMe()
+    {
+        foreach (var user in users)
+        {
+            // Fazer o login e obter o token
+            var loginResponse = await client.PostAsync("https://localhost:7011/api/users/login", new StringContent(
+                JsonConvert.SerializeObject(new { email = user.Email, password = user.Password }),
+                Encoding.UTF8,
+                "application/json"
+            ));
+
+            loginResponse.EnsureSuccessStatusCode();
+            var loginResponseBody = await loginResponse.Content.ReadAsStringAsync();
+            var loginData = JsonConvert.DeserializeObject<LoginResponse>(loginResponseBody);
+            var token = loginData?.Token;
+
+            // Verificar os dados do usuário autenticado
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7011/api/users/me")
+            {
+                Headers =
+                {
+                    { "Authorization", $"Bearer {token}" }
+                }
+            };
+
+            var userResponse = await client.SendAsync(requestMessage);
+
+            userResponse.EnsureSuccessStatusCode();
+            Console.WriteLine($"User data for {user.Email} retrieved successfully.");
+        }
+    }
+}
+
+
+public class User
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+
+public class LoginResponse
+{
+    public string Token { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<ApiBenchmark>();
+    }
+}
